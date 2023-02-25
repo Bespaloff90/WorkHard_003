58606392d50618f39425f8708908f6e9c867e0d6
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-25 15:27:40 +0300

    add second

---
+++ WH2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Hello!");
+Console.WriteLine("Enter First numbers:     ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second number:      ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Three number:        ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max)  max = b;
+if (c > max)  max = c;
+
+Console.Write(max);
+
