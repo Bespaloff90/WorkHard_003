1bfb8fb33e634db9f2ef6f4dd1af8790334f20ed
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-25 15:21:37 +0300

    add a>b>c

--- WH1/Program.cs
+++ WH1/Program.cs
@@ -9,7 +9,6 @@ Console.WriteLine("Enter first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter second number:  ");
 int b = Convert.ToInt32(Console.ReadLine());
-
 if( a > b)
 {
     Console.WriteLine("first number > second number");
