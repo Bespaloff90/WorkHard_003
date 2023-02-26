1980d4e1e042ece2a19cfe6c0a46b3d3ba454c1d
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-26 10:56:21 +0300

    add only even

---
+++ WN4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Enter your number:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = 1;
+// even <= number; even++)
+while (even <= number)
+
+{ 
+
+    if (even % 2 == 0)
+    {
+        Console.WriteLine(even);
+        
+    }
+
+even++;
+}
+
